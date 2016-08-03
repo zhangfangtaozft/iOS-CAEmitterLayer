607adbbd6f78db19bdd1d015e626cf4992772b7e
zft <894918941@qq.com>
2016-08-03 17:01:25 +0800

    iOS-CAEmitterLayer
    视频直播中的粒子效果。

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by 张芳涛 on 16/8/3.
+//  Copyright © 2016年 张芳涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by 张芳涛 on 16/8/3.
+//  Copyright © 2016年 张芳涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
